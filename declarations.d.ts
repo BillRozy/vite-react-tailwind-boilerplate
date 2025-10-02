55a4524721f26594a4b0d5968e0aa4e1ab993105
Oleg Budylin <billrozy@gmail.com>
2025-10-02 17:15:36 +0200

    inital version of boilerplate

---
+++ declarations.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.less";
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
